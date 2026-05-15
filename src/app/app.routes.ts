0a54c6940c1855053ad16325bc1a29b1dd0b98b8
Srdan Topalovic <srdan@elke.studio>
2026-05-15 10:44:08 +0200

    chore: push from ng0 canvas (2026-05-15T08:44:08.125Z)

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,6 +2,6 @@ import { Routes } from '@angular/router';
 
 export const routes: Routes = [
   { path: '', redirectTo: 'home', pathMatch: 'full' },
-  { path: 'home', loadComponent: () => import('./screens/home/home.component').then(m => m.HomeComponent) },
-  { path: 'about-us', loadComponent: () => import('./screens/about-us/about-us.component').then(m => m.AboutUsComponent) },
+  { path: 'home', loadComponent: () => import('./pages/home/home').then(m => m.HomeComponent) },
+  { path: 'about-us', loadComponent: () => import('./pages/about-us/about-us').then(m => m.AboutUsComponent) },
 ];
