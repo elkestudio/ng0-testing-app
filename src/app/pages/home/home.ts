6db0b639619fad3934df294e83f46ce2053ab302
Srdan Topalovic <srdan@elke.studio>
2026-05-18 08:35:16 +0200

    updares

--- src/app/pages/home/home.ts
+++ src/app/pages/home/home.ts
@@ -2,13 +2,13 @@ import { ChangeDetectionStrategy, Component, signal } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 import { RouterLink, RouterOutlet } from '@angular/router';
-import { Ng0ButtonComponent, Ng0ChipComponent, Ng0ContentComponent, Ng0DockComponent, Ng0HeaderComponent, Ng0IconComponent, Ng0InputComponent, Ng0RatingComponent, Ng0TextComponent } from '@ng0/ui';
+import { Ng0ButtonComponent, Ng0ChipComponent, Ng0ContentComponent, Ng0DockComponent, Ng0HeaderComponent, Ng0IconComponent, Ng0InputComponent, Ng0TextComponent } from '@ng0/ui';
 
 @Component({
   selector: 'app-home',
   standalone: true,
   changeDetection: ChangeDetectionStrategy.OnPush,
-  imports: [CommonModule, FormsModule, RouterLink, RouterOutlet, Ng0ButtonComponent, Ng0ChipComponent, Ng0ContentComponent, Ng0DockComponent, Ng0HeaderComponent, Ng0IconComponent, Ng0InputComponent, Ng0RatingComponent, Ng0TextComponent],
+  imports: [CommonModule, FormsModule, RouterLink, RouterOutlet, Ng0ButtonComponent, Ng0ChipComponent, Ng0ContentComponent, Ng0DockComponent, Ng0HeaderComponent, Ng0IconComponent, Ng0InputComponent, Ng0TextComponent],
   templateUrl: './home.ng.html'})
 export class HomeComponent {
   protected readonly loading = signal(true);
