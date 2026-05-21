fedb8eb60cfe09918cd5585bbaa624d857899205
Srdan Topalovic <srdan@elke.studio>
2026-05-21 14:31:59 +0200

    chore: push from ng0 canvas (2026-05-21T12:31:58.891Z)

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -2,11 +2,11 @@ import { ApplicationConfig } from '@angular/core';
 import { provideRouter } from '@angular/router';
 import { provideTheme } from '@ng0/ui';
 import { routes } from './app.routes';
-import { PURPLE_MOON } from './theme/theme.config';
+import { PURPLE_MOON, PALENIGHT } from './theme/theme.config';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideRouter(routes),
-    provideTheme({ themes: [PURPLE_MOON], prefersLight: PURPLE_MOON, prefersDark: PURPLE_MOON }),
+    provideTheme({ themes: [PURPLE_MOON, PALENIGHT], prefersLight: PURPLE_MOON, prefersDark: PURPLE_MOON }),
   ],
 };
--- src/app/theme/theme.config.ts
+++ src/app/theme/theme.config.ts
@@ -1,3 +1,4 @@
 import { defineTheme } from '@ng0/ui';
 
 export const PURPLE_MOON = defineTheme({ name: 'Purple Moon', isDark: true });
+export const PALENIGHT = defineTheme({ name: 'Palenight', isDark: true });
